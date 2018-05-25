462f7a1b1b95e720cddecf90a204f405e9a2e052
Daniel Paiva <dmjpaiva@gmail.com>
2018-05-25 11:21:17 -0300

    Habilitando edição de itens

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,5 +1,7 @@
 class ProdutosController < ApplicationController
 
+    before_action :set_produto, only: [:edit, :update, :destroy]
+
     def index
         @produtos_por_nome = Produto.order(:nome)
         @produtos_por_preco = Produto.order(:preco)
@@ -10,13 +12,13 @@ class ProdutosController < ApplicationController
       # :produto é o mesmo que "produto", só que sem gerar string na memória
       # permit serve pra permitir qualquer coisa que vier de ser pega
       # os parâmetros depois de permit são os únicos atributos de Produto que ele aceitará
-      valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
+      valores = produto_params
       @produto = Produto.new valores
       if @produto.save
         flash[:notice] = "Produto salvo com sucesso"
         redirect_to root_url
       else
-        render :new
+        renderiza_new
       end
     end
 
@@ -26,14 +28,42 @@ class ProdutosController < ApplicationController
     end
 
     def destroy
-      id = params[:id]
-      Produto.destroy id
+      @produto.destroy
       redirect_to root_url
     end
 
     def busca
       @nome_a_buscar = params[:nome]
       @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
+    end
+
+    def edit
+      renderiza_new :edit
+    end
+
+    def update
+      if @produto.update produto_params
+        flash[:notice] = "Produto atualizado com sucesso"
+        redirect_to root_url
+      else
+        renderiza :edit
+      end
+    end
+
+    private # a partir daqui tudo fica privado
+
+    def renderiza_new (view)
+      @departamentos = Departamento.all
+      render view
+    end
+
+    def set_produto
+      id = params[:id]
+      @produto = Produto.find(id)
+    end
+
+    def produto_params
+      params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :departamentos
     # resources gera todas as routes padrão necessárias, incluindo POST, GET, DELETE, GET. Para vê-las, utilize
     # rake routes no terminal. Ele pode também receber o que criar, como abaixo.
-    resources :produtos, only: [:new, :create, :destroy]
+    resources :produtos, only: [:new, :create, :destroy, :edit, :update]
     get "/produtos/busca" => "produtos#busca", as: :busca_produto
     root "produtos#index"
 end
